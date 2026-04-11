024667018232626fae1a54b9ac105ae1e0c8ed25
LooKeR <iamlooker@proton.me>
2026-04-11 23:13:20 +0530

    chore: Release 1.3.3
    Signed-off-by: LooKeR <iamlooker@proton.me>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.3.3] - 2026-04-11
+
 ### Added
 - Backup and restore for whole data
 - Activity graph on the Profile screen
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.looker.kenko"
         minSdk = 26
         targetSdk = 36
-        versionName = "1.3.2"
+        versionName = "1.3.3"
         versionCode = versionCodeFor(versionName)
 
         testInstrumentationRunner = "com.looker.kenko.KenkoTestRunner"
